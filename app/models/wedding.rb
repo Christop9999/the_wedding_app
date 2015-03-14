b97dfeb51997fe46d275ce4cf0e05cd8551dfeef
Chris Murolo <crmurolo@gmail.com>
2015-03-14 01:02:00 -0700

    changed inflection for API, wrote tests for create

--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -1,4 +1,9 @@
 class Wedding < ActiveRecord::Base
 	validates :name, presence: true,
 				     length: {minimum: 3}
+	validates :bride, presence: true,
+				     length: {minimum: 2}
+	validates :groom, presence: true,
+				     length: {minimum: 2}
+
 end
--- spec/features/weddings/create_spec.rb
+++ spec/features/weddings/create_spec.rb
@@ -9,6 +9,9 @@ describe "Creating weddings" do
 		fill_in "Name", with: "WeddingName"
 		fill_in "Bride", with: "BrideName"
 		fill_in "Groom", with: "GroomName"
+		select '2015', :from => 'wedding[date(1i)]'
+		select 'March', :from => 'wedding[date(2i)]'
+		select '14', :from => 'wedding[date(3i)]'
 		
 		click_button "Create Wedding"
 
@@ -26,6 +29,10 @@ describe "Creating weddings" do
 		fill_in "Name", with: ""
 		fill_in "Bride", with: "BrideName"
 		fill_in "Groom", with: "GroomName"
+
+		select '2015', :from => 'wedding[date(1i)]'
+		select 'March', :from => 'wedding[date(2i)]'
+		select '14', :from => 'wedding[date(3i)]'
 		
 		click_button "Create Wedding"
 
@@ -44,6 +51,98 @@ describe "Creating weddings" do
 		fill_in "Name", with: "Hi"
 		fill_in "Bride", with: "BrideName"
 		fill_in "Groom", with: "GroomName"
+
+		select '2015', :from => 'wedding[date(1i)]'
+		select 'March', :from => 'wedding[date(2i)]'
+		select '14', :from => 'wedding[date(3i)]'
+		
+		click_button "Create Wedding"
+
+		expect(page).to have_content ("error")
+		expect(Wedding.count).to eq (0)
+
+	end
+
+	it "displays an error when the bride has no name" do
+		expect(Wedding.count).to eq(0)
+
+		visit "/weddings"
+		click_link "New Wedding"
+		expect(page).to have_content ("New wedding")
+		
+		fill_in "Name", with: "Wedding"
+		fill_in "Bride", with: ""
+		fill_in "Groom", with: "GroomName"
+
+		select '2015', :from => 'wedding[date(1i)]'
+		select 'March', :from => 'wedding[date(2i)]'
+		select '14', :from => 'wedding[date(3i)]'
+		
+		click_button "Create Wedding"
+
+		expect(page).to have_content ("error")
+		expect(Wedding.count).to eq (0)
+
+	end
+
+	it "displays an error when the Brides name has less than 3 characters" do
+		expect(Wedding.count).to eq(0)
+
+		visit "/weddings"
+		click_link "New Wedding"
+		expect(page).to have_content ("New wedding")
+		
+		fill_in "Name", with: "Wedding"
+		fill_in "Bride", with: "B"
+		fill_in "Groom", with: "GroomName"
+
+		select '2015', :from => 'wedding[date(1i)]'
+		select 'March', :from => 'wedding[date(2i)]'
+		select '14', :from => 'wedding[date(3i)]'
+		
+		click_button "Create Wedding"
+
+		expect(page).to have_content ("error")
+		expect(Wedding.count).to eq (0)
+
+	end
+
+	it "displays an error when the groom has no name" do
+		expect(Wedding.count).to eq(0)
+
+		visit "/weddings"
+		click_link "New Wedding"
+		expect(page).to have_content ("New wedding")
+		
+		fill_in "Name", with: "Wedding"
+		fill_in "Bride", with: "BrideName"
+		fill_in "Groom", with: ""
+
+		select '2015', :from => 'wedding[date(1i)]'
+		select 'March', :from => 'wedding[date(2i)]'
+		select '14', :from => 'wedding[date(3i)]'
+		
+		click_button "Create Wedding"
+
+		expect(page).to have_content ("error")
+		expect(Wedding.count).to eq (0)
+
+	end
+
+	it "displays an error when the groom name has less than 3 characters" do
+		expect(Wedding.count).to eq(0)
+
+		visit "/weddings"
+		click_link "New Wedding"
+		expect(page).to have_content ("New wedding")
+		
+		fill_in "Name", with: "Wedding"
+		fill_in "Bride", with: "BrideName"
+		fill_in "Groom", with: "G"
+
+		select '2015', :from => 'wedding[date(1i)]'
+		select 'March', :from => 'wedding[date(2i)]'
+		select '14', :from => 'wedding[date(3i)]'
 		
 		click_button "Create Wedding"
 
