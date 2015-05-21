7fb51bc4cf7604cd23ae994fcc27e5f236e83afa
christop9999 <crmurolo@gmail.com>
2015-05-21 01:15:13 -0700

    latest pic

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 module WeddingApp
   class Application < Rails::Application
-    
+    config.serve_static_assets = true
 
       # Enable the asset pipeline
     #config.assets.enabled = true
