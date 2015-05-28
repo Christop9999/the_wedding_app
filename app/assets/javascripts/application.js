d55d6b1b40f1d5a8d52ff577a10a27a0a55aff32
christop9999 <crmurolo@gmail.com>
2015-05-28 01:05:54 -0700

    precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
+//= require jquery.turbolinks
 //= require turbolinks
 //= require_tree .
-//= require bootstrap
-//= require jquery.turbolinks
+
+
