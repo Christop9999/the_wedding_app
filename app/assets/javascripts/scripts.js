9b8fd8163077576003838247dd3b36acb981fd03
christop9999 <crmurolo@gmail.com>
2015-05-28 01:14:08 -0700

    adjusted js

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,5 +1,11 @@
+var ready;
+ready = function() {
+	
 $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
+}
+$(document).ready(ready);
+$(document).on('page:load', ready);
