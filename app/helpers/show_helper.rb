efd245d7cac1368e6de334a7cfcccafe08dbbc75
christop9999 <crmurolo@gmail.com>
2015-06-25 21:24:22 -0700

    drop tables and re-migrated

--- app/helpers/show_helper.rb
+++ app/helpers/show_helper.rb
@@ -1,7 +1,10 @@
 module ShowHelper
 	def show_user_bg
-		"background-image: url(#{@wedding.content.header.url});"
-
+		if @wedding.content.blank?
+			"background-image: url(#{asset_path('BG1.jpg')});"
+		else
+			"background-image: url(#{@wedding.content.header.url});"
+		end
 	end
 
 end
